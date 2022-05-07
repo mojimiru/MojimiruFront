180fb850e9046642c5564e1ab8ba15fa1f55a6b3
mojimiru <mojimiru.dev@gmail.com>
2022-05-08 03:50:39 +0900

    Updates

---
+++ js/about.56a7c3bf.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmojimiru"]=self["webpackChunkmojimiru"]||[]).push([[443],{402:function(n,u,r){r.r(u),r.d(u,{default:function(){return l}});var i=r(252);const t={class:"about"},a=(0,i._)("h1",null,"This is an about page",-1),e=[a];function s(n,u){return(0,i.wg)(),(0,i.iD)("div",t,e)}var c=r(744);const o={},f=(0,c.Z)(o,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.56a7c3bf.js.map
