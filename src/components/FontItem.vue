12a7129f83936938244e0ca5a5b95ab41168db42
mojimiru <mojimiru.dev@gmail.com>
2022-05-09 20:02:03 +0900

    ✨ タグによるフィルタを実装

--- src/components/FontItem.vue
+++ src/components/FontItem.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import { ref } from '@vue/reactivity'
-import { defineProps, defineExpose } from 'vue'
+import { defineProps, defineExpose, watch, toRefs } from 'vue'
 const props = defineProps<{
   content: string,
   fontId: string;
-  fontName: string;
+  fontData: {name:string, types:string[], weight:number, letters:string[], tags:string[], desc:string|undefined};
   width: number;
   height: number;
+  show:boolean;
+  tagMap:{[key:string]:string}
 }>()
 
 const color = ref('black')
 const fontsize = ref(30)
 // let span:HTMLElement | null = null
 
-const reflesh = (charsURI: string) => {
+const chars = new Set()
+const reloadFont = (text:string) => {
+  if (!props.show) { return }
+
+  const newChars = Array.from(new Set(text)).filter(c => (!chars.has(c)))
+
+  if (newChars.length === 0) { return }
+  console.log(newChars)
+
+  newChars.forEach(c => chars.add(c))
+  const charsURI = encodeURIComponent(newChars.join(''))
+
   color.value = 'gray'
   console.log('fontsize.value')
   const fontURL = `https://mojimiru.pythonanywhere.com?font=${props.fontId}&text=${charsURI}`
@@ -32,9 +45,9 @@ const reflesh = (charsURI: string) => {
   })
 }
 
-defineExpose({ reflesh })
-
-// onMounted(() => { span = document.getElementById(props.fontId) })
+const { show } = toRefs(props)
+watch(show, (value) => { value && reloadFont(props.content) })
+defineExpose({ reloadFont })
 </script>
 
 <template>
@@ -47,9 +60,16 @@ defineExpose({ reflesh })
       <!-- <v-spacer/> -->
     </v-card-title>
     <v-spacer/>
-    <v-card-text class="text-caption align-self-end d-flex flex-column pt-1 pb-1" style="bottom: 0">
+    <v-card-text class="text-caption align-self-end d-flex flex-column pt-1 pb-0" style="bottom: 0">
       <v-spacer/>
-      {{fontName}}
+      {{fontData.name}}
+    </v-card-text>
+    <v-card-text class="text-caption align-self-end d-flex flex-column" style="bottom: 0">
+      <v-row>
+        <v-chip v-for="a in fontData.tags" :key="a">
+          {{tagMap[a]}}
+        </v-chip>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
--- src/components/FontList.vue
+++ src/components/FontList.vue
@@ -1,56 +1,78 @@
 <script setup lang="ts">
-import { ref, reactive } from '@vue/reactivity'
+import { ref, reactive, computed } from '@vue/reactivity'
 import FontItem from '@/components/FontItem.vue'
 import { onBeforeUpdate, onMounted } from 'vue'
+import * as fontJson from '@/assets/fonts.json'
 const activeContent = ref('ためしがき')
 const content = ref('')
 
-const fonts = reactive(
-  {
-    kaisotai_next: '廻想体 ネクスト ユーピー（B）',
-    marukoias_alpha: 'まるこいあすα',
-    hangyaku: '叛逆明朝',
-    soukou_mincho: '装甲明朝'
-  })
+console.log(fontJson)
+
+const fonts = fontJson.fonts
 const fontRefs = ref([])
 
 onBeforeUpdate(() => {
   fontRefs.value = []
 })
 
-let letters = new Set()
 const reflesh = () => {
   content.value = activeContent.value
-  const newLetters = new Set(content.value)
-  const unloadedLetters = Array.from(newLetters).filter(c => (!letters.has(c)))
-
-  if (unloadedLetters.length === 0) { return }
-  console.log(unloadedLetters)
-
-  letters = new Set([...Array.from(letters), ...Array.from(unloadedLetters)])
   fontRefs.value.forEach(el => {
-    el.reflesh(encodeURIComponent(unloadedLetters.join('')))
+    el.reloadFont(content.value)
   })
 }
-
 onMounted(() => reflesh())
+
+const tagMap = Object.fromEntries(fontJson.tags.flatMap(Object.entries))
+
+const includeMap = computed(() => [...fonts.keys()].map(includes))
+const tagGroups = fontJson.tags.map(tags => Object.keys(tags))
+const selectedTags = ref(tagGroups.map(() => []))
+const includes = (index:number) => selectedTags.value.every((tags, i) => !tags.length || tags.some(tag => fonts[index].tags.includes(tagGroups[i][tag])))
 </script>
 
 <template>
   <v-container fluid>
-    <textarea type="text" id="text" required size="10" v-model="activeContent" @change="reflesh"/>
+    <v-textarea
+    label="試したい単語/文章を入力"
+    v-model="activeContent"
+    @change="reflesh"
+    dense
+    auto-grow
+    rows="1"
+    />
+    <div v-for="(group, i) in tagGroups" :key="i">
+      <v-chip-group column multiple active-class="primary--text" v-model="selectedTags[i]">
+        <v-chip
+          v-for="tag of group"
+          :key="tag"
+        >
+          {{ fontJson.tags[i][tag] }}
+        </v-chip>
+      </v-chip-group>
+    </div>
     <v-row dense>
-      <v-col
-        v-for="(fontName, fontId, j) in fonts"
-        :key="fontId"
-        cols="12"
-        lg="3"
-        md="4"
-        sm="6"
-        xs="12"
-      >
-        <FontItem :ref="el => {if (el) fontRefs[j] = el}" :width="300" :height="100" :content="content" :fontId="fontId" :fontName="fontName"></FontItem>
-      </v-col>
+      <template v-for="(font, i) in fonts" :key="i">
+        <v-col
+          v-show="includeMap[i]"
+          cols="12"
+          lg="3"
+          md="4"
+          sm="6"
+          xs="12"
+        >
+          <FontItem
+          :ref="el => {if (el) fontRefs[i] = el}"
+          :show="includeMap[i]"
+          :width="300"
+          :height="100"
+          :content="content"
+          :fontId="font.id"
+          :fontData="font"
+          :tagMap="tagMap"
+          />
+        </v-col>
+      </template>
     </v-row>
   </v-container>
 </template>
