71ca09e87b0f26472b4c3c112ad6abe3fd10b00d
mojimiru <mojimiru.dev@gmail.com>
2022-05-11 22:59:12 +0900

    Updates

---
+++ js/about.e5255fb1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmojimiru"]=self["webpackChunkmojimiru"]||[]).push([[443],{4901:function(n,u,r){r.r(u),r.d(u,{default:function(){return l}});var i=r(9269);const t={class:"about"},a=(0,i._)("h1",null,"This is an about page",-1),e=[a];function s(n,u){return(0,i.wg)(),(0,i.iD)("div",t,e)}var c=r(7617);const o={},f=(0,c.Z)(o,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.e5255fb1.js.map
