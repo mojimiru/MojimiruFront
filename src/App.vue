1faea5eb365f1d3c15eb67a9ed3c3be2bcc7b647
mojimiru <mojimiru.dev@gmail.com>
2022-05-12 12:25:45 +0900

    changed title to ぱらぐりふ

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,17 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header :class="`bg-${$q.dark.mode?'black':'white'}`">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div id="input" class="row items-center">
-            <span id="cursor" class="text-transparent col-0">{{ activeContent }}<span :class="$q.dark.mode?'text-white':'text-black'">|</span></span>
-            <q-input :color="$q.dark.mode?'white':'black'" class="q-pa-sm col-12" dense v-model="activeContent" bg-color="transparent"
-              @change="change" />
-          </div>
-        </q-toolbar-title>
-        <q-btn dense :color="$q.dark.mode?'white':'black'" icon="filter_alt" class="q-mr-sm" @click="drawer = !drawer" outline></q-btn>
-        <q-btn dense :color="$q.dark.mode?'white':'black'" :icon="$q.dark.mode?'dark_mode':'light_mode'" class="q-mr-sm" @click="$q.dark.toggle" outline></q-btn>
-        <q-btn dense :color="$q.dark.mode?'white':'black'" icon="question_mark" class="q-mr-sm" @click="questionDialog = true" outline></q-btn>
-      </q-toolbar>
-    </q-header>
-    <q-page-container>
-      <FontList ref="fontlist" :text="activeContent" :selected="selectedTags" :tagMap="tagMap" />
-      <q-dialog full-width v-model="drawer">
-        <q-card class="q-pa-md">
-          <div class="row items-start">
-            <q-input class="q-pa-sm grow" :color="$q.dark.mode?'white':'black'" label="表示したい文章を入力" style="font-size: 1.2rem;" clearable
-              v-model="activeContent" outlined autogrow @change="change" />
-            <q-btn :color="$q.dark.mode?'white':'black'" icon="close" flat round dense v-close-popup />
-          </div>
-          <div v-for="(tags, i) in fontJson.tags" :key="i" class="q-pa-sm col-auto">
-            <ChipGroup v-model="selectedTags[i]"
-              :options="Object.entries(tags).map(v => ({ value: v[0], label: v[1] }))">
-            </ChipGroup>
-          </div>
-        </q-card>
-      </q-dialog>
-    </q-page-container>
-  </q-layout>
-</template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import FontList from './components/FontList.vue'
 import * as fontJson from '@/assets/fonts.json'
 import ChipGroup from './components/ChipGroup.vue'
+import AboutDialog from './components/AboutDialog.vue'
 import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
 
-const questionDialog = ref(false)
+const about = ref(false)
 
-const activeContent = ref('ParaGlyph')
+const activeContent = ref('ぱらぐりふ')
 const fontlist = ref(null)
 const drawer = ref(false)
 const change = () => {
@@ -73,6 +39,45 @@ onMounted(() => {
   change()
 })
 </script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header :class="`bg-${$q.dark.mode?'black':'white'}`">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div id="input" class="row items-center">
+            <span id="cursor" class="text-transparent col-0">{{ activeContent }}<span :class="$q.dark.mode?'text-white':'text-black'">|</span></span>
+            <q-input :color="$q.dark.mode?'white':'black'" class="q-pa-sm col-12" dense v-model="activeContent" bg-color="transparent"
+              @change="change" />
+          </div>
+        </q-toolbar-title>
+        <q-btn dense :color="$q.dark.mode?'white':'black'" icon="filter_alt" class="q-mr-sm" @click="drawer = !drawer" outline></q-btn>
+        <q-btn dense :color="$q.dark.mode?'white':'black'" :icon="$q.dark.mode?'dark_mode':'light_mode'" class="q-mr-sm" @click="$q.dark.toggle" outline></q-btn>
+        <q-btn dense :color="$q.dark.mode?'white':'black'" icon="question_mark" class="q-mr-sm" @click="about = true" outline></q-btn>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <FontList ref="fontlist" :text="activeContent" :selected="selectedTags" :tagMap="tagMap" />
+      <q-dialog full-width v-model="drawer">
+        <q-card class="q-pa-md">
+          <div class="row items-start">
+            <q-input class="q-pa-sm grow" :color="$q.dark.mode?'white':'black'" label="表示したい文章を入力" style="font-size: 1.2rem;" clearable
+              v-model="activeContent" outlined autogrow @change="change" />
+            <q-btn :color="$q.dark.mode?'white':'black'" icon="close" flat round dense v-close-popup />
+          </div>
+          <div v-for="(tags, i) in fontJson.tags" :key="i" class="q-pa-sm col-auto">
+            <ChipGroup v-model="selectedTags[i]"
+              :options="Object.entries(tags).map(v => ({ value: v[0], label: v[1] }))">
+            </ChipGroup>
+          </div>
+        </q-card>
+      </q-dialog>
+      <q-dialog v-model="about">
+       <about-dialog/>
+      </q-dialog>
+    </q-page-container>
+  </q-layout>
+</template>
 
 <style>
 * {
---
+++ src/components/AboutDialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card class="q-pa-md" style="width: 700px; max-width: 80vw;">
+    <q-card-section align="left" class="q-pa-sm col-12 row">
+      <span class="text-h5">
+        ぱらぐりふについて
+      </span>
+    </q-card-section>
+    <q-separator/>
+    <q-card-section align="left" class="q-pa-md col-12">
+      <pre style="white-space: pre-wrap; font-family: sans-sarif;">ぱらぐりふ-ParaGlyph-は日本語対応の商用利用可能なフォントだけを集めたフォント比較サイトです。
+画面上部の入力欄に好きな文字を入れるとそれぞれのフォントで表示されます。
+気に入ったフォントがあればクリックして、配布ページからダウンロードしてくださいね！</pre>
+    </q-card-section>
+</q-card>
+</template>
